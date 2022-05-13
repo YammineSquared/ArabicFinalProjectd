34ef0d6062f7cc29408e3b44f54039e8f0ab49a7
Yammine Yammine <yammine_yammine@hotmail.com>
2022-05-14 01:03:43 +0300

    final touches

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <iframe src="/index.html" ></iframe>
+  return ( 
+    <>
+      <Head>
+        <title>محمد علي شاكر</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <iframe src="/index.html" ></iframe>
+    </>     
   )
 }
